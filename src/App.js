931968e1271401d615b3420adb82a7b4106b0ec0
Ricky_Home <ricky860526@gmail.com>
2025-05-19 23:55:56 +0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+//外部套件
+import logo from "./assets/logo.svg";
+import "./assets/App.css";
+import Input from "./components/Input";
+import "./assets/all.scss";
 
 function App() {
+  const [text, setText] = useState("");
+  const onChangeHandler = (e) => {
+    setText(e.target.value);
+  };
+  useEffect(() => {
+    (async () => {
+      // console.log(process.env.REACT_APP_PATH);
+      const path = process.env.REACT_APP_PATH;
+
+      const result = await axios.get(path);
+      console.log(result);
+    })();
+    return;
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          loremwlkfnlkwneflkwenflknwkefnlkwnklfnklwnefklwnlkfnwlkn
+          wlfkjlwkjlkwjeflkwjeflkjwlkfjkwjefjkwefjlkw
+          wlkjeflkwjeflkwjeflkwjeflkjwe
         </p>
         <a
           className="App-link"
@@ -17,6 +38,16 @@ function App() {
         >
           Learn React
         </a>
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+        {text}
+        <Input
+          id="sampleText"
+          text="這是一˙個input"
+          value={text}
+          onChangeHandler={onChangeHandler}
+        ></Input>
       </header>
     </div>
   );
